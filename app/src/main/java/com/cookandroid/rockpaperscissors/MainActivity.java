bd2d4e4cfc59f8f9df0fd067616708684120237c
한상희 <tkdgml822@gmail.com>
2023-09-27 00:07:35 +0900

    가위바위보 구현 완료

--- app/src/main/java/com/cookandroid/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/cookandroid/rockpaperscissors/MainActivity.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button scissorsButton, rockButton, paperButton;
-    EditText playerText;
+    EditText playerText, result, computerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +19,93 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         playerText = findViewById(R.id.playerText);
+        computerText = findViewById(R.id.computer);
+
         scissorsButton = findViewById(R.id.scissor);
         rockButton = findViewById(R.id.rock);
         paperButton = findViewById(R.id.paper);
 
+        result = findViewById(R.id.result);
+
         scissorsButton.setOnClickListener(view -> {
             playerText.setText("가위");
+            result(computer(1));
+
         });
 
         rockButton.setOnClickListener(view -> {
             playerText.setText("바위");
+            result(computer(2));
         });
 
         paperButton.setOnClickListener(view -> {
             playerText.setText("보");
+            result(computer(3));
         });
     }
-    private int random() {
-        Random rd = new Random();
 
-        return (rd.nextInt(3) + 1);
+    // 1 : 가위, 2 : 바위 : ,3 : 보
+    private int computer(int human) {
+        Random rd = new Random();
+        int randomNumber = (rd.nextInt(3) + 1);
+        if (randomNumber == 1) {
+            computerText.setText("가위");
+        }
+        else if (randomNumber == 2) {
+            computerText.setText("바위");
+        }
+        else {
+            computerText.setText("보");
+        }
+
+        switch (human) {
+            // 사용자가 가위를 냈을 때
+            case 1:
+                if (randomNumber == 2) {
+                    return 0;
+                }
+                else if (randomNumber == 3) {
+                    return 1;
+                }
+                else {
+                    return 3;
+                }
+            // 사용자가 바위를 냈을 때
+            case 2:
+                if (randomNumber == 1) {
+                    return 1;
+                }
+                else if (randomNumber == 3) {
+                    return 0;
+                }
+                else {
+                    return 3;
+                }
+            // 사용자가 보를 냈을 때
+            case 3:
+                if (randomNumber == 1) {
+                    return 0;
+                }
+                else if (randomNumber == 2) {
+                    return 1;
+                }
+                else {
+                    return 3;
+                }
+        }
+        return  0;
+    }
+
+    private void result(int num) {
+        if (num == 0) {
+            result.setText("졌습니다.");
+        }
+        else if (num == 1) {
+            result.setText("이겼습니다.");
+        }
+        else {
+            result.setText("비겼습니다.");
+        }
     }
 
 }
