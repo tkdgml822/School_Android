9ff6a82daccedd417769e4684b98eb75842b06ee
한상희 <tkdgml822@gmail.com>
2023-10-31 15:49:28 +0900

    유저 정보 입력 화면

--- app/src/main/java/com/cookandroid/rockpaperscissors/Intro.java
+++ app/src/main/java/com/cookandroid/rockpaperscissors/Intro.java
@@ -2,13 +2,23 @@ package com.cookandroid.rockpaperscissors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intro extends AppCompatActivity {
-
+    Button gameStartButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        gameStartButton = findViewById(R.id.gameStartButton);
+
+        gameStartButton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), PlayerNameInput.class);
+            startActivity(intent);
+        });
     }
 }
