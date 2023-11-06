9b01d019e81a86eb051ac963eac4248eec198b78
한상희 <tkdgml822@gmail.com>
2023-11-06 22:23:48 +0900

    PlayerNameInput Button edit

--- app/src/main/java/com/cookandroid/rockpaperscissors/Intro.java
+++ app/src/main/java/com/cookandroid/rockpaperscissors/Intro.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,12 +14,12 @@ public class Intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
         gameStartButton = findViewById(R.id.gameStartButton);
 
         gameStartButton.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), PlayerNameInput.class);
-            startActivity(intent);
+            Intent next = new Intent(getApplicationContext(), PlayerNameInput.class);
+            startActivity(next);
         });
+
     }
 }
--- app/src/main/java/com/cookandroid/rockpaperscissors/PlayerNameInput.java
+++ app/src/main/java/com/cookandroid/rockpaperscissors/PlayerNameInput.java
@@ -2,13 +2,26 @@ package com.cookandroid.rockpaperscissors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PlayerNameInput extends AppCompatActivity {
+    Button nextButton;
+    TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_name_input);
+        nextButton = findViewById(R.id.nextButton);
+        userName = findViewById(R.id.userName);
+
+        Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+        nextButton.setOnClickListener(v -> {
+            mainIntent.putExtra("name", userName.getText().toString());
+            startActivity(mainIntent);
+        });
     }
 }
