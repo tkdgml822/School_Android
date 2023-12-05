b708c6e6096d48650df4f493848bcacaf5e89b96
한상희 <tkdgml822@gmail.com>
2023-12-05 16:55:41 +0900

    앱 종료 버튼 구현

--- app/src/main/java/com/cookandroid/rockpaperscissors/EndingActivity.java
+++ app/src/main/java/com/cookandroid/rockpaperscissors/EndingActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class EndingActivity extends AppCompatActivity {
-    Button previousButton;
+    Button previousButton, appCloseButton;
     TextView nameText, winRateText, winCountText, loseCountText;
 
     @Override
@@ -23,7 +23,8 @@ public class EndingActivity extends AppCompatActivity {
         nameText = findViewById(R.id.ending_name); // 이름
         winRateText = findViewById(R.id.ending_win_rate); // 승률
         winCountText = findViewById(R.id.ending_win_count); // 승
-        loseCountText = findViewById(R.id.ending_lose_count); // ㅍ
+        loseCountText = findViewById(R.id.ending_lose_count); // 패
+        appCloseButton = findViewById(R.id.app_close); // 앱 종료
 
 
         // 넘겨진 데이터 받아오기
@@ -45,7 +46,11 @@ public class EndingActivity extends AppCompatActivity {
             finish();
         });
 
-
+        appCloseButton.setOnClickListener(view -> {
+            moveTaskToBack(true); // 태스크를 백그라운드로 이동
+            finishAndRemoveTask(); // 액티비티 종료 + 태스크 리스트에서 지우기
+            android.os.Process.killProcess(android.os.Process.myPid()); // 앱 프로세스 종료
+        });
 
     }
 }
