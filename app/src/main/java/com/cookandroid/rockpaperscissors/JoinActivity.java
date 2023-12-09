4ad91e005625f88a38b55d3151ca23654816095e
한상희 <tkdgml822@gmail.com>
2023-12-09 15:58:39 +0900

    Join Activtiy 구현

--- app/src/main/java/com/cookandroid/rockpaperscissors/JoinActivity.java
+++ app/src/main/java/com/cookandroid/rockpaperscissors/JoinActivity.java
@@ -1,14 +1,62 @@
 package com.cookandroid.rockpaperscissors;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.cookandroid.rockpaperscissors.LoginActivity;
+import com.cookandroid.rockpaperscissors.MainActivity;
+import com.cookandroid.rockpaperscissors.MembershipOpenHelper;
+
 
 public class JoinActivity extends AppCompatActivity {
 
+    MembershipOpenHelper openHelper;
+    SQLiteDatabase db;
+    EditText emailEt, pwdEt;
+    Button joinBtn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+        openHelper = new MembershipOpenHelper(this);
+        db = openHelper.getWritableDatabase();
+        emailEt = (EditText) findViewById(R.id.emailEt2);
+        pwdEt = (EditText) findViewById(R.id.pwdEt2);
+        joinBtn = (Button) findViewById(R.id.joinBtn2);
+        joinBtn.setOnClickListener(listener);
     }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.joinBtn2) {
+                String email = emailEt.getText().toString();
+                String pwd = pwdEt.getText().toString();
+                String sql = "select * from membership where email = '"+email+"'";
+                Cursor cursor = db.rawQuery(sql, null);
+                if(cursor.getCount() == 1) {
+                    // 해당 이메일과 아이디가 있으면 1개의 row를 가져오겠죠?
+                    Toast.makeText(JoinActivity.this, "이미 존재하는 이메일입니다.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(JoinActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    // 없다면 아무 값도 가져오지 않으므로 count 가 0 이겠죠?
+                    String sql2 ="insert into membership(email, pwd) values('"+email+"','"+pwd+"')";
+                    db.execSQL(sql2);
+                    Toast.makeText(JoinActivity.this, "회원가입을 축하합니다.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(JoinActivity.this, LoginActivity.class));
+                }
+                cursor.close();
+            }
+        }
+    };
 }
